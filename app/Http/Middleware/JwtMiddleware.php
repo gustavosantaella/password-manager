4f5c7fc71627ada09381ddfe480bf9b153a6b7f1
Gustavo Alejandro <llxsantaellaxll@gmail.com>
2023-10-15 08:56:42 -0400

    fix: middleware

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -21,14 +21,14 @@ class JwtMiddleware extends BaseMiddleware
     public function handle($request, Closure $next)
     {
         try {
-            $user = JWTAuth::parseToken()->authenticate();
+
+            JWTAuth::parseToken()->authenticate();
+
         } catch (Exception $e) {
             if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
-                return response()->json(['status' => 'Token is Invalid']);
+                return response()->json(['status' => 400, 'message' => 'Token is Invalid'])->setStatusCode(400);
             }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
-                return response()->json(['status' => 'Token is Expired']);
-            }else{
-                return response()->json(['status' => 'Authorization Token not found']);
+                return response()->json(['status' => 400, 'message'=> 'Token is Expired'])->setStatusCode(400);
             }
         }
         return $next($request);
